34e44fab88527e04163646357b19b732f1a0b941
saqib92 <najam1992@gmail.com>
2018-12-21 12:56:54 +0500

    video work

--- src/pages/video/video.ts
+++ src/pages/video/video.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ToastController, LoadingController } from 'ionic-angular';
 import { HobbiesPage } from '../hobbies/hobbies';
 import { DashboardPage } from '../dashboard/dashboard';
 import { TestimonialPage } from '../testimonial/testimonial';
 import { OptionsPage } from '../options/options';
 import { ProfilePage } from '../profile/profile';
 import { UploadvideoPage } from '../uploadvideo/uploadvideo';
-
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+import { globalData } from '../../helper/helper';
+import { Storage } from '@ionic/storage';
 /**
  * Generated class for the VideoPage page.
  *
@@ -20,15 +23,45 @@ import { UploadvideoPage } from '../uploadvideo/uploadvideo';
   templateUrl: 'video.html',
 })
 export class VideoPage {
-
+loader:any;
+headers:any;
+myVids:any;
+vidUrl:any;
   constructor(
     public navCtrl: NavController,
-    public navParams: NavParams
-    ) {
+    public navParams: NavParams,
+    public http: Http, 
+    private toastCtrl: ToastController,
+    public loadingCtrl: LoadingController,
+    private storage: Storage
+    )  {
+    this.vidUrl = globalData.imagesUrl;
+    this.storage.get('userData').then((val)=>{
+      this.getVideos(val.id);
+    })
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad VideoPage');
+  }
+
+  presentToast(message) {
+    let toast = this.toastCtrl.create({
+      message: message,
+      duration: 3000,
+      position: 'bottom'
+    });
+
+    toast.present();
+  }
+
+  presentLoadingDefault() {
+    this.loader = this.loadingCtrl.create({
+      content: 'Please wait...'
+    });
+
+    this.loader.present();
+    
   }
 
   tohobbies() {
@@ -49,6 +82,27 @@ export class VideoPage {
 
   toProfile(){
     this.navCtrl.setRoot(ProfilePage);
+  }
+
+  getVideos(id){
+
+    this.presentLoadingDefault();
+    this.headers = {'Content-Type':'application/json'};
+      this.http.get(globalData.serviceUrl + 'video/index/'+id, {headers: this.headers})
+       .map(res => res.json())
+       .subscribe(data => {
+         console.log(data);
+       if(data.status ==true) {
+         this.myVids = data.data;
+         this.loader.dismiss();
+         this.presentToast(data.message);
+
+       } else {
+         this.loader.dismiss();
+         this.presentToast(data.message)
+       }
+      });
+
   }
 
   
